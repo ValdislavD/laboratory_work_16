6ebca3003e596cfa5dc0895c2e8c7192519cf7bf
VladislavD <vladislavdmuhov792@gmail.com>
2024-05-07 17:17:24 +0300

    seventeenth_task

--- eleventh16_task.c
+++ eleventh16_task.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+/*#include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include "tasks/string_.h"
@@ -156,4 +156,4 @@ void testFindPreviousWord() {
 int main() {
     testFindPreviousWord();
     return 0;
-}
+}*/
